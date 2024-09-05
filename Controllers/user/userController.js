88939777c1a7d7dab28181ef4593554ce3bc2f9c
Karen Huamán <karmahucents@gmail.com>
2024-09-05 15:25:05 -0500

    servicio de correo completado

--- Controllers/user/userController.js
+++ Controllers/user/userController.js
@@ -1,54 +1,66 @@
-const userdb   = require('../../Service/authService/Serviceusers')
-const { validateUser } = require('../../Models/users');
-const CustomError = require('../../Service/errors');
-const { existEmail,existEmailUpdate } = require('../common');
+const userdb = require("../../Service/authService/Serviceusers");
+const { validateUser } = require("../../Models/users");
+const CustomError = require("../../Service/errors");
+const { existEmail, existEmailUpdate } = require("../common");
+const { sendEmail } = require("../../Service/authService/NewPassword");
 
 module.exports = {
-    getAllUsers: async(req, resp, next) => {
-        try {
-            const users = await userdb.getAllUsers();
-            resp.send({ result: users });
-        } catch (err) {
-            resp.status(500).send({ statusCode: 500, message: err.message });
-        }          
-        
-    },
-    createUser: async(req, resp, next) => {
-        try { 
-            const newUser = validateUser(req.body);
-
-            if (await existEmail(req.body.correo)) {
-                throw new CustomError({ correo: ['email already exists']}, 400);
-            }
-
-            let result = await userdb.createUser(newUser);
-            resp.send({ result });
-
-        } catch (err) {
-            if (err instanceof CustomError) {
-                return next(err);  
-            }
-            resp.status(500).send({ statusCode: 500, message: err.message });
-        } 
-    },
-
-    updateUser: async(req, resp, next) => {
-        try {
-            console.log(req.body)
-            const updateUser = validateUser(req.body);
-
-            if(await existEmailUpdate(req.body.ids)){
-                throw new CustomError({ correo: ['email already exists']}, 400);
-            }
-
-            let result = await userdb.updateUser(updateUser)
-            resp.send({ result });
-        } catch (err) {
-            if (err instanceof CustomError) {
-                return next(err);  
-            }
-            resp.status(500).send({ statusCode: 500, message: err.message });
-        }
+  getAllUsers: async (req, resp, next) => {
+    try {
+      const users = await userdb.getAllUsers();
+      resp.send({ result: users });
+    } catch (err) {
+      resp.status(500).send({ statusCode: 500, message: err.message });
     }
-    
+  },
+  createUser: async (req, resp, next) => {
+    try {
+      const newUser = validateUser(req.body);
+
+      if (await existEmail(req.body.correo)) {
+        throw new CustomError({ correo: ["email already exists"] }, 400);
+      }
+
+      let result = await userdb.createUser(newUser);
+      resp.send({ result });
+    } catch (err) {
+      if (err instanceof CustomError) {
+        return next(err);
+      }
+      resp.status(500).send({ statusCode: 500, message: err.message });
+    }
+  },
+
+  updateUser: async (req, resp, next) => {
+    try {
+      console.log(req.body);
+      const updateUser = validateUser(req.body);
+
+      if (await existEmailUpdate(req.body.ids)) {
+        throw new CustomError({ correo: ["email already exists"] }, 400);
+      }
+
+      let { userId, userEmail } = await userdb.updateUser(updateUser);
+      console.log('Usuario actualizado con ID:', userId);
+
+      const subject = 'Tu rol ha sido actualizado';
+      const text = 'Tu rol ha sido cambiado en la plataforma. Ya puedes ingresar con tu nuevo rol.';
+      const html = `
+          <h1>Tu rol ha sido actualizado</h1>
+          <p>Hola ${updateUser.nombre},</p>
+          <p>Tu rol ha sido actualizado en la plataforma y ahora puedes acceder a todas las funcionalidades asociadas a tu nuevo rol.</p>
+          <p>Por favor, ingresa a la plataforma y verifica tus permisos.</p>
+          <p>Saludos,</p>
+          <p>Equipo de Soporte</p>
+      `;
+      const emailResult = await sendEmail(userEmail, subject, text, html);
+      console.log("Resultado del envio de correo: ", emailResult);
+      resp.send({ userId, emailResult });
+    } catch (err) {
+      if (err instanceof CustomError) {
+        return next(err);
+      }
+      resp.status(500).send({ statusCode: 500, message: err.message });
+    }
+  },
 };
--- Service/authService/Serviceusers.js
+++ Service/authService/Serviceusers.js
@@ -1,47 +1,103 @@
-const oracledb = require('oracledb');
-const db = require('../../Settings/Database/database');
+const oracledb = require("oracledb");
+const db = require("../../Settings/Database/database");
 
 module.exports = {
-    getAllUsers: async() => {
-        const data = { cursor: { type: oracledb.CURSOR, dir: oracledb.BIND_OUT }};
-        const users = await db.procedureExecuteCursor(`BEGIN PG_SCAI_CONSULTA.PA_SCAI_USUARIOS(:cursor); END;`, data);
-        return users.cursor;
-    },
-    existEmail: async(email) => {
-        const user = await db.procedureExecuteCursor(`BEGIN PG_SCAI_CONSULTA.PA_SCAI_GENERIC_SELECT_EXECUTE(:sql_stmt,:cursor); END;`, {
-            sql_stmt: `SELECT CUSU_EMAIL FROM SCAI_USUARIOS WHERE CUSU_EMAIL='${email}'`,
-            cursor: { type: oracledb.CURSOR, dir: oracledb.BIND_OUT }
-        });
-        return user.cursor[0];
-    },
-    existEmailUpdate: async(email,ids) => {
-        const user = await db.procedureExecuteCursor(`BEGIN PG_SCAI_CONSULTA.PA_SCAI_GENERIC_SELECT_EXECUTE(:sql_stmt,:cursor); END;`, {
-            sql_stmt: `SELECT CUSU_EMAIL FROM SCAI_USUARIOS WHERE CUSU_EMAIL='${email}' AND NUSU_ID!='${ids}'`,
-            cursor: { type: oracledb.CURSOR, dir: oracledb.BIND_OUT }
-        });
-        return user.cursor[0];
-    },
-    createUser: async(data) => {
-        data.ids = { type: oracledb.NUMBER, dir: oracledb.BIND_OUT };
-        const newEvent = await db.procedureExecute(`BEGIN PG_SCAI_CONSULTA.PA_SCAI_INSERT_USUARIO(
+  getAllUsers: async () => {
+    const data = { cursor: { type: oracledb.CURSOR, dir: oracledb.BIND_OUT } };
+    const users = await db.procedureExecuteCursor(
+      `BEGIN PG_SCAI_CONSULTA.PA_SCAI_USUARIOS(:cursor); END;`,
+      data
+    );
+    return users.cursor;
+  },
+  existEmail: async (email) => {
+    const user = await db.procedureExecuteCursor(
+      `BEGIN PG_SCAI_CONSULTA.PA_SCAI_GENERIC_SELECT_EXECUTE(:sql_stmt,:cursor); END;`,
+      {
+        sql_stmt: `SELECT CUSU_EMAIL FROM SCAI_USUARIOS WHERE CUSU_EMAIL='${email}'`,
+        cursor: { type: oracledb.CURSOR, dir: oracledb.BIND_OUT },
+      }
+    );
+    return user.cursor[0];
+  },
+  existEmailUpdate: async (email, ids) => {
+    const user = await db.procedureExecuteCursor(
+      `BEGIN PG_SCAI_CONSULTA.PA_SCAI_GENERIC_SELECT_EXECUTE(:sql_stmt,:cursor); END;`,
+      {
+        sql_stmt: `SELECT CUSU_EMAIL FROM SCAI_USUARIOS WHERE CUSU_EMAIL='${email}' AND NUSU_ID!='${ids}'`,
+        cursor: { type: oracledb.CURSOR, dir: oracledb.BIND_OUT },
+      }
+    );
+    return user.cursor[0];
+  },
+  createUser: async (data) => {
+    data.ids = { type: oracledb.NUMBER, dir: oracledb.BIND_OUT };
+    const newEvent = await db.procedureExecute(
+      `BEGIN PG_SCAI_CONSULTA.PA_SCAI_INSERT_USUARIO(
             :apellido,
             :correo,
             :sexo,
             :ids,
             :nombre,
             :pais
-            ); END;`, data); 
-        return newEvent.ids;    
-    },
-    updateUser: async(data) => {
-        data.ids = { type: oracledb.NUMBER, dir: oracledb.BIND_INOUT ,val:data.ids};
-        const newEvent = await db.procedureExecute(`BEGIN PG_SCAI_CONSULTA.PA_SCAI_UPDATE_USUARIO(
-            :apellido,
-            :sexo,
-            :ids,
-            :nombre,
-            :pais,
-            :rol); END;`, data); 
-        return newEvent.ids;    
+            ); END;`,
+      data
+    );
+    return newEvent.ids;
+  },
+  updateUser: async (data) => {
+    try {
+      const bindings = {
+        apellido: { val: data.apellido, type: oracledb.STRING },
+        sexo: { val: data.sexo, type: oracledb.STRING },
+        ids: { dir: oracledb.BIND_INOUT, val: data.ids, type: oracledb.NUMBER },
+        nombre: { val: data.nombre, type: oracledb.STRING },
+        pais: { val: data.pais, type: oracledb.NUMBER },
+        rol: { val: data.rol, type: oracledb.NUMBER },
+      };
+
+      await db.procedureExecute(
+        `
+                BEGIN 
+                    PG_SCAI_CONSULTA.PA_SCAI_UPDATE_USUARIO(
+                        :apellido,
+                        :sexo,
+                        :ids,
+                        :nombre,
+                        :pais,
+                        :rol
+                    ); 
+                END;`,
+        bindings,
+        {
+          autoCommit: true,
+        }
+      );
+
+      const userId = bindings.ids.val;
+
+      const userResult = await db.simpleExecute(
+        `
+            SELECT cusu_email
+            FROM scai_usuarios
+            WHERE nusu_id = :id
+        `,
+        { id: userId },
+        { outFormat: oracledb.OUT_FORMAT_OBJECT }
+      );
+
+      const userEmail = userResult.rows.length
+        ? userResult.rows[0].CUSU_EMAIL
+        : null;
+
+      if (!userEmail) {
+        throw new Error("No se pudo encontrar el correo del usuario.");
+      }
+
+      return { userId, userEmail };
+    } catch (err) {
+      console.error("Error al ejecutar el procedimiento", err);
+      throw new Error(err.message);
     }
-}
+  },
+};
