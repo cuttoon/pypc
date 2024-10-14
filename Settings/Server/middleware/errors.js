9a9d16bf45df2c0a6f2bba445deb1b656e1b3caf
Karen Huamán <karmahucents@gmail.com>
2024-10-13 22:15:52 -0500

    cambiando docs en seguimiento

--- Settings/Server/middleware/errors.js
+++ Settings/Server/middleware/errors.js
@@ -1,27 +1,25 @@
 const httpErrors = {
-    400: 'Bad request',
-    401: 'Unauthorized',
-    403: 'Forbidden',
-    404: 'Not found',
-    500: 'Internal server error',
+  400: "Bad request",
+  401: "Unauthorized",
+  403: "Forbidden",
+  404: "Not found",
+  500: "Internal server error",
 };
-  
 
 const isKnownHTTPErrorStatus = (num) => {
-    return typeof num === 'number' && Object.keys(httpErrors).indexOf(`${num}`) >= 0;
+  return (
+    typeof num === "number" && Object.keys(httpErrors).indexOf(`${num}`) >= 0
+  );
 };
-   
 
 module.exports = (err, req, resp, next) => {
-    const statusCode = (isKnownHTTPErrorStatus(err))
-        ? err
-        : err.statusCode || 500;
+  const statusCode = isKnownHTTPErrorStatus(err) ? err : err.statusCode || 500;
 
-    const message = err.message || httpErrors[statusCode] || err;
-  
-    if (statusCode === 500) {
-        console.error(statusCode, message);
-    }  
-    resp.status(statusCode).json({ statusCode, message });
-    next();
+  const message = err.message || httpErrors[statusCode] || err;
+
+  if (statusCode === 500) {
+    console.error(statusCode, message);
+  }
+  resp.status(statusCode).json({ statusCode, message });
+  next();
 };
